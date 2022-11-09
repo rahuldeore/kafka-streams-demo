0a95a934ef1e6292514492d42fbd60f70fb3449d
Rahul Deore <rahul.deore@outlook.com>
2022-11-08 19:33:34 -0500

    First commit

---
+++ src/test/java/com/rahul/kafka/wordcount/WordCountStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.kafka.wordcount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCountStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
